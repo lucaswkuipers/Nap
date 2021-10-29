2ef80310e9be144ff96276075290b1446fb35dee
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2021-10-29 19:59:47 -0300

    Remove unnecessary template code

--- NapTests/NapTests.swift
+++ NapTests/NapTests.swift
@@ -1,33 +1,13 @@
-//
-//  NapTests.swift
-//  NapTests
-//
-//  Created by Lucas Kuipers on 29/10/21.
-//
-
 import XCTest
 @testable import Nap
 
 class NapTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    override func setUpWithError() throws {}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    override func tearDownWithError() throws {}
 
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        self.measure{}
     }
-
 }
