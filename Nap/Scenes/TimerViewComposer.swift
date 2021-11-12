8534a4868cb3034c1f3562afe9aee48688ea2092
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2021-11-11 22:17:14 -0300

    Remove unused code

--- Nap/Scenes/TimerViewComposer.swift
+++ Nap/Scenes/TimerViewComposer.swift
@@ -2,8 +2,6 @@ import UIKit
 
 struct TimerViewComposer {
     static func makeScene() -> UIViewController {
-//        let view = UIView()
-//        view.backgroundColor = .red
         let view = TimerView()
         let viewController = BaseViewController(from: view)
 
