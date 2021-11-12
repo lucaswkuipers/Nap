415d6e4bab101f11915e38f96555176b426c0093
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2021-11-11 22:16:55 -0300

    Move timer button to its own file

---
+++ Nap/Scenes/TimerButton.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+final class TimerButton: UIButton {
+    override var isHighlighted: Bool {
+        didSet {
+            backgroundColor = isHighlighted ? .secondaryLabel : .tertiaryLabel
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        alpha = 0.8
+        titleLabel?.font = .systemFont(ofSize: 40)
+        layer.cornerRadius = 20
+        backgroundColor = .tertiaryLabel
+        setTitleColor(.label, for: .normal)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
--- Nap/Scenes/TimerView.swift
+++ Nap/Scenes/TimerView.swift
@@ -1,26 +1,5 @@
 import UIKit
 
-final class TimerButton: UIButton {
-    override var isHighlighted: Bool {
-        didSet {
-            backgroundColor = isHighlighted ? .secondaryLabel : .tertiaryLabel
-        }
-    }
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-
-        alpha = 0.8
-        titleLabel?.font = .systemFont(ofSize: 40)
-        layer.cornerRadius = 20
-        backgroundColor = .tertiaryLabel
-        setTitleColor(.label, for: .normal)
-    }
-
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-    }
-}
-
 final class TimerView: UIView {
     let verticalStackView: UIStackView = {
         let stackView = UIStackView()
